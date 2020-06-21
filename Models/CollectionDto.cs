d41063da9ef35904ac9aed951cbc1007a5d7dfc6
4lexRossi <devalexrossi@gmail.com>
2020-06-21 08:11:12 -0300

    Api com 2 collections Usarios e Processos conectadas ao MongoDB Atlas

---
+++ Models/CollectionDto.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Api.Models
+{
+    public class UsuarioDto
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Password { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+
+    }public class ProcessoDto
+    {
+        public string Id { get; set; }
+        public string NomeCliente { get; set; }
+        public string NumProcesso { get; set; }
+        public DateTime DataDecisao { get; set; }
+        public string Descricao { get; set; }
+        public string ProximoPasso { get; set; }
+        public string LinkProcesso { get; set; }
+
+    }
+}
